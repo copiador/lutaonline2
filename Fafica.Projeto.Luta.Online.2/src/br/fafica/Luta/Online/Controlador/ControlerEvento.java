9a572fbff8029afb2ac5a805bcc844aba537ffca
copiador <jessemarquesads@gmail.com>
2013-11-06 12:07:10 -0300

    Algumas Classes

--- Fafica.Projeto.Luta.Online.2/src/br/fafica/Luta/Online/Controlador/ControlerEvento.java
+++ Fafica.Projeto.Luta.Online.2/src/br/fafica/Luta/Online/Controlador/ControlerEvento.java
@@ -1,5 +1,14 @@
 package br.fafica.Luta.Online.Controlador;
 
+import br.fafica.Luta.Online.Dao.RepositorioLutadorJDBC;
+
 public class ControlerEvento {
+	
+	private RepositorioLutadorJDBC rp;
+	
+	
+	public ControlerEvento() {
+		rp = new RepositorioLutadorJDBC();
+	}
 
 }
